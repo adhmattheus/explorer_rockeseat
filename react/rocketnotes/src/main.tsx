91c635b742af59b753061358a05f51e43ffed98a
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-01-20 18:48:13 -0300

    feat: signUp page

--- react/rocketnotes/src/main.tsx
+++ react/rocketnotes/src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.ts";
 import GlobalStyles from "./styles/global.ts";
-import { SignIn } from "./pages/SignIn/index.tsx";
+import { SignUp } from "./pages/SignUp/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </StrictMode>
 );
